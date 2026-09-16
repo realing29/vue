0c4653c82a64c5270febbcf574e61c0f07cda626
@r.vasilaki <r.vasilaki@diagramm.ru>
2026-09-16 19:49:12 +0500

    Добавлены алиасы

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@app": fileURLToPath(new URL("./src/app", import.meta.url)),
+      "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
+      "@widgets": fileURLToPath(new URL("./src/widgets", import.meta.url)),
+      "@features": fileURLToPath(new URL("./src/features", import.meta.url)),
+      "@entities": fileURLToPath(new URL("./src/entities", import.meta.url)),
+      "@shared": fileURLToPath(new URL("./src/shared", import.meta.url)),
+    },
+  },
+});
