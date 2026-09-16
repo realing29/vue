d88e1d1742c1e892e6151b6e5cf540893559cfe0
@r.vasilaki <r.vasilaki@diagramm.ru>
2026-09-17 04:58:09 +0500

    удален не используемая переменная

--- src/shared/i18n/index.ts
+++ src/shared/i18n/index.ts
@@ -1,7 +1,6 @@
 import { createI18n } from "vue-i18n";
 import ru from "./locales/ru";
 
-export const LOCALE_STORAGE_KEY = "app-locale";
 export const SUPPORTED_LOCALE = "ru";
 
 export const i18n = createI18n({
